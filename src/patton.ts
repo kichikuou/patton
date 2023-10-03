14deb52fb8eb34e8f78716f9c2ceeecd7336e757
kichikuou <KichikuouChrome@gmail.com>
2023-10-03 20:50:33 +0900

    Redirect to '/' if game fails to boot

--- src/patton.ts
+++ src/patton.ts
@@ -28,6 +28,7 @@ class ImageManager {
         try {
             return this.idbfs.FS.readFile('/patton/' + imageName);
         } catch (err) {
+            this.idbfs.FS.readFile('/patton/ADISK.PAT');  // throws if it doesn't exist
             const image = new Uint8Array(await buildHDImage(this.idbfs));
             this.persist(image);
             return image;
@@ -45,6 +46,7 @@ class App {
     private imageMan = new ImageManager();
 
     async main() {
+        const image = await this.imageMan.loadOrBuild();
         this.np2 = await NP2.create({
             canvas: $('#canvas') as HTMLCanvasElement,
             clk_mult: 8,
@@ -54,7 +56,7 @@ class App {
             onDiskChange: this.onDiskChange.bind(this),
             onExit: () => history.back(),
         });
-        this.np2.addDiskImage(imageName, await this.imageMan.loadOrBuild());
+        this.np2.addDiskImage(imageName, image);
         this.np2.setHdd(0, imageName);
         this.np2.run();
     }
@@ -69,4 +71,6 @@ class App {
 }
 
 zoom.initialize();
-new App().main();
+new App().main().catch(() => {
+    location.href = '/';
+});
